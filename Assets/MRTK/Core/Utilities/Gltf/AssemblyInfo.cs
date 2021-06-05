a02a926d6fc819e2764481e3c02927a7d513b16c
Yoon Park <cre8ive.park@gmail.com>
2021-06-04 20:37:45 -0700

    Update to MRTK 2.7 + Unity 2019

--- Assets/MRTK/Core/Utilities/Gltf/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Microsoft.MixedReality.Toolkit.Gltf.Importers")]
